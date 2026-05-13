232b43d39d6dd8e9a802ad43032fdf788231529a
Sam Gutentag <1404219+samgutentag@users.noreply.github.com>
2026-05-13 12:31:47 -0700

    feat: define shared types for snapshots and KV values

---
+++ src/shared/types.ts
@@ -0,0 +1,47 @@
+export type SystemInfo = {
+  system_id: string
+  name: string
+  timezone: string
+  language: string
+}
+
+export type StationStatic = {
+  station_id: string
+  name: string
+  lat: number
+  lon: number
+  address?: string
+}
+
+export type StationDynamic = {
+  station_id: string
+  num_bikes_available: number
+  num_docks_available: number
+  bikes_electric: number
+  bikes_classic: number
+  bikes_smart: number
+  is_installed: boolean
+  is_renting: boolean
+  is_returning: boolean
+  last_reported: number
+}
+
+export type StationSnapshot = StationStatic & StationDynamic
+
+export type KVValue = {
+  system: SystemInfo
+  snapshot_ts: number
+  stations: StationSnapshot[]
+}
+
+export type BufferEntry = {
+  snapshot_ts: number
+  stations: StationDynamic[]
+}
+
+export class NormalizeError extends Error {
+  constructor(message: string, public field?: string) {
+    super(message)
+    this.name = 'NormalizeError'
+  }
+}
